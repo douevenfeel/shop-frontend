b22cb3e5dfdbb5b6e2900f6a6d632409267462dd
whereayo <mdxnilovd@gmail.com>
2022-11-30 01:49:28 +0300

    pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
+import { App } from './App';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { App } from './App';
 import { Provider } from 'react-redux';
-import { store } from 'store';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from 'store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -12,5 +12,14 @@ root.render(
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </Provider>
+    </Provider>,
 );
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker
+            .register('/serviceWorker.js')
+            .then((response) => console.log('service worker registered'))
+            .catch((error) => console.log('service worker not registered', error));
+    });
+}
