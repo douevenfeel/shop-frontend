f87b2c02d0f91dadffc8fd0cac70f994c480e155
douevenfeel <mdxnilovd@gmail.com>
2022-11-29 15:55:05 +0300

    manager order panel

--- src/pages/ManagerUserPage/ManagerUserPage.tsx
+++ src/pages/ManagerUserPage/ManagerUserPage.tsx
@@ -1,46 +1,21 @@
 import { ManagerUserPageProps } from './ManagerUserPage.types';
 import { ManagerOrderCard } from 'components/ManagerOrderCard';
+import { ManagerOrderPanel } from 'components/ManagerOrderPanel';
 import { Navigation } from 'components/Navigation';
-import { OrderPanel } from 'components/OrderPanel';
 import { useAppDispatch, useAppSelector } from 'hooks/redux';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchGetAllOrdersManagerAction } from 'store/actions/orderAction';
 import { changeUserPageOrder } from 'store/reducers/orderReducer';
-import { Container, Paragraph } from 'utils/styles';
-
-type CurrentType = 'all' | 'delivered' | 'in delivery' | 'canceled';
+import { Container } from 'utils/styles';
 
 export const ManagerUserPage: React.FC<ManagerUserPageProps> = () => {
     const dispatch = useAppDispatch();
     const { id } = useParams();
-    const { userPage, orders, fetchActionStatus, count, limit } = useAppSelector((store) => store.order);
-
-    const [params, setParams] = useState({});
-    const [current, setCurrent] = useState<CurrentType>('all');
+    const { userPage, orders, fetchActionStatus, count, limit, params } = useAppSelector((store) => store.order);
 
     const handlePage = (page: number) => {
         dispatch(changeUserPageOrder(page));
-    };
-
-    const handleAll = () => {
-        setParams({});
-        setCurrent('all');
-    };
-
-    const handleDelivered = () => {
-        setParams({ delivered: true });
-        setCurrent('delivered');
-    };
-
-    const handleInDelivery = () => {
-        setParams({ delivered: false, canceled: false });
-        setCurrent('in delivery');
-    };
-
-    const handleCanceled = () => {
-        setParams({ canceled: true });
-        setCurrent('canceled');
     };
 
     useEffect(() => {
@@ -57,18 +32,8 @@ export const ManagerUserPage: React.FC<ManagerUserPageProps> = () => {
             gap='12px'
             width='80vw'
         >
-            <OrderPanel
-                handleAll={handleAll}
-                handleCanceled={handleCanceled}
-                handleDelivered={handleDelivered}
-                handleInDelivery={handleInDelivery}
-                current={current}
-            />
-            {orders.length !== 0 ? (
-                orders.map((order) => <ManagerOrderCard key={order.id} order={order} />)
-            ) : (
-                <Paragraph>user don't have {current} orders</Paragraph>
-            )}
+            <ManagerOrderPanel />
+            {orders.length !== 0 && orders.map((order) => <ManagerOrderCard key={order.id} order={order} />)}
             <Navigation count={count} limit={limit} page={userPage} handlePage={handlePage} />
         </Container>
     );
